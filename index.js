6a26ff52468784e1af4107689bd58089e53fd169
Furkan <furkancelebiii9@gmail.com>
2026-03-11 00:42:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+const express = require("express");
+const app = express();
+
+// Port Railway otomatik atıyor, yoksa 3000 kullan
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World from Railway!");
+});
+
+// guncelle
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
